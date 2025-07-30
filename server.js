b57f35858c46d6532727338963150d18cc006696
flower2p68 <164519238+flower2p68@users.noreply.github.com>
2025-07-30 12:19:39 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import { Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+const app = express();
+app.use(express.json());
+
+app.get('/usuarios', async(req, res) => {
+    let users = [];
+    if(req.query){
+        users= await prisma.user.findMany({ 
+            where:{
+                name:req.query.name,
+                age:req.query.age,
+                email:req.query.email
+            }
+        })}else{
+            users=await prisma.user.findMany()
+        }
+    res.status(200).json(users)
+})
+
+app.delete('/usuarios/:id', async(req,res)=>{
+    await prisma.user.delete({
+    where:{
+        id:req.params.id
+    }
+})
+    res.status(200).json('Usuario deletado')
+})
+
+app.post('/usuarios', async(req,res)=>{
+    await prisma.user.create({
+        data: {
+        age:req.body.age,
+        email:req.body.email,
+        name:req.body.name
+    }
+})
+    res.status(201).json(req.body)
+})
+
+app.put('/usuarios/:id', async(req,res)=>{
+    await prisma.user.update({
+        where:{
+            id:req.params.id
+        },
+        data:{
+            age:req.body.age,
+            email:req.body.email,
+            name:req.body.name
+        }
+        
+    })
+    res.status(201).json(req.body)
+})
+
+
+app.listen(5050, function() {
+    console.log("servidor rodando");
+  });
